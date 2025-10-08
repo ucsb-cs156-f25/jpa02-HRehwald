9012574ea3f2e497c9598bf007ef77ea1cc5e290
hauke_rehwald <hauke_rehwald@ucsb.edu>
2025-10-07 22:17:03 -0700

    full mutation coverage test cases added

--- src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
@@ -24,9 +24,24 @@ public class DeveloperTest {
     public void getName_returns_correct_name() {
         // TODO: Replace Chris G. with your name as shown on
         // <https://bit.ly/cs156-f25-teams>
-        assertEquals("Chris G.", Developer.getName());
+        assertEquals("Hauke", Developer.getName());
     }
+    @Test
+    public void getGithubId_returns_correct_githubId() {
+    	assertEquals("HRehwald", Developer.getGithubId());
+    }
+    @Test
+    public void getTeam_returns_team_with_correct_name() {
+        Team  t = Developer.getTeam();
+        assertEquals("f25-12", t.getName());
+	assertTrue(t.getMembers().contains("Josh"),"Team should contain Josh");
+        assertTrue(t.getMembers().contains("Hauke"),"Team should contain Hauke");
+        assertTrue(t.getMembers().contains("Tej"),"Team should contain Tej");
+        assertTrue(t.getMembers().contains("Jayden"),"Team should contain Jayden");
+        assertTrue(t.getMembers().contains("Ben"),"Team should contain Bed");
+        assertTrue(t.getMembers().contains("Alvin"),"Team should contain Alvin");
 
+    }
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
 
--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -8,10 +8,13 @@ import org.junit.jupiter.api.Test;
 public class TeamTest {
 
     Team team;
-
+    Team team2;
+    Team team3;
     @BeforeEach
     public void setup() {
-        team = new Team("test-team");    
+        team = new Team("test-team");
+    	team2 = new Team("test-team");
+	team3 = new Team("bad-tean");	
     }
 
     @Test
@@ -22,5 +25,36 @@ public class TeamTest {
    
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
+    @Test
+    public void toString_returns_correct_string() {
+        assertEquals("Team(name=test-team, members=[])", team.toString());
+    }
+
+    @Test 
+    public void equals_same_object() {
+    	assertEquals(team.equals(team), true);
+    }
+
+    @Test
+    public void equals_different_type() {
+    	assertEquals(team.equals("theATeam"), false);
+    }
+
+    @Test
+    public void equals_check_fields() {
+
+	assert(team.equals(team2));
+	
+	assertEquals(team.equals(team3), false);
+    
+    }
+
+    @Test
+    public void hashCode_test() {
+    	
+	assert(team.hashCode() == team2.hashCode());
+	
+	assertEquals(team.hashCode() == team3.hashCode(), false);
+    }
 
 }
