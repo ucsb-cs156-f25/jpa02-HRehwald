7cf1ddd405f0c9afc5ee5b44c8f5e915a5154fce
hauke_rehwald <hauke_rehwald@ucsb.edu>
2025-10-08 18:29:22 -0700

    changed names tests

--- src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
@@ -24,7 +24,7 @@ public class DeveloperTest {
     public void getName_returns_correct_name() {
         // TODO: Replace Chris G. with your name as shown on
         // <https://bit.ly/cs156-f25-teams>
-        assertEquals("Hauke", Developer.getName());
+        assertEquals("HAUKE SEBASTIAN", Developer.getName());
     }
     @Test
     public void getGithubId_returns_correct_githubId() {
@@ -34,12 +34,12 @@ public class DeveloperTest {
     public void getTeam_returns_team_with_correct_name() {
         Team  t = Developer.getTeam();
         assertEquals("f25-12", t.getName());
-	assertTrue(t.getMembers().contains("Josh"),"Team should contain Josh");
-        assertTrue(t.getMembers().contains("Hauke"),"Team should contain Hauke");
-        assertTrue(t.getMembers().contains("Tej"),"Team should contain Tej");
-        assertTrue(t.getMembers().contains("Jayden"),"Team should contain Jayden");
-        assertTrue(t.getMembers().contains("Ben"),"Team should contain Bed");
-        assertTrue(t.getMembers().contains("Alvin"),"Team should contain Alvin");
+	assertTrue(t.getMembers().contains("JOSHUA CHACKO"),"Team should contain Josh");
+        assertTrue(t.getMembers().contains("HAUKE SEBASTIAN"),"Team should contain Hauke");
+        assertTrue(t.getMembers().contains("TEJ SRI"),"Team should contain Tej");
+        assertTrue(t.getMembers().contains("JAYDEN JUN KHAI"),"Team should contain Jayden");
+        assertTrue(t.getMembers().contains("BEN"),"Team should contain Bed");
+        assertTrue(t.getMembers().contains("ALVIN KEN"),"Team should contain Alvin");
 
     }
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
